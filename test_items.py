e1c0646f308b774fbe3c6fd50aff8ffff69531b8
nastiesdi <nastiesdi>
2020-01-16 12:14:50 +0300

    'for_review'

---
+++ test_items.py
@@ -0,0 +1,17 @@
+
+import time
+
+
+link ='http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+
+
+class TestLogin(object):
+    def test_is_button_exist(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector("#login_link")
+        browser.get(link)
+        time.sleep(2)
+        button = browser.find_element_by_class_name("btn-add-to-basket")
+        assert button
+
+
