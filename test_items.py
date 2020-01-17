bbf79f808c0f523e23287b603859f8728a46b3b0
nastiesdi <nastiesdi>
2020-01-17 12:43:45 +0300

    'program_ready'

--- test_items.py
+++ test_items.py
@@ -2,14 +2,12 @@
 import time
 
 
-link ='http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+link ='http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
 class TestLogin(object):
     def test_is_button_exist(self, browser):
         browser.get(link)
-        # browser.find_element_by_css_selector("#login_link")
-        # browser.get(link)
         time.sleep(10)
         button = browser.find_element_by_class_name("btn-add-to-basket")
         assert button
